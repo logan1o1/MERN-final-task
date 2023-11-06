44b9a5267678e0d0d9c153a1eaa0e14c6c808839
Akshat <akshat9950@gmail.com>
2023-11-06 15:42:40 +0530

    New Login Box Added

--- Final-task-MERN/src/components/Header.jsx
+++ Final-task-MERN/src/components/Header.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-import {Link, NavLink} from 'react-router-dom'
+Header.jsx
+
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Login from "../components/Login"; // Import the Login component
 
 export default function Header() {
+  const [showLogin, setShowLogin] = useState(false); // State to control login dialog
+
+  const openLoginDialog = () => {
+    setShowLogin(true);
+  };
+
+  const closeLoginDialog = () => {
+    setShowLogin(false);
+  };
+
   return (
     <header className="shadow sticky z-50 top-0">
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
@@ -14,12 +27,13 @@ export default function Header() {
             />
           </Link>
           <div className="flex items-center lg:order-2">
-            <NavLink
-              to="#"
-              className="text-gray-800 hover:bg-gray-800 hover:text-orange-400 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+            <a
+              href="#"
+              className="text-gray-800 hover:bg-gray-800 hover-text-orange-400 focus:ring-4 focus: ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              onClick={openLoginDialog}
             >
               Log in
-            </NavLink>
+            </a>
           </div>
           <div
             className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
@@ -28,9 +42,11 @@ export default function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <NavLink
-                to="/"
-                  className={({isActive}) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-600" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  to="/"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${
+                      isActive ? "text-orange-600" : "text-gray-700"
+                    } border-b border-gray-100 hover-bg-gray-50 lg:hover-bg-transparent lg-border-0 hover-text-orange-700 lg-p-0`
                   }
                 >
                   Home
@@ -38,9 +54,11 @@ export default function Header() {
               </li>
               <li>
                 <NavLink
-                to="/todayItems"
-                  className={({isActive}) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-600" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  to="/todayItems"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${
+                      isActive ? "text-orange-600" : "text-gray-700"
+                    } border-b border-gray-100 hover-bg-gray-50 lg:hover-bg-transparent lg-border-0 hover-text-orange-700 lg-p-0`
                   }
                 >
                   Today's Items
@@ -48,9 +66,11 @@ export default function Header() {
               </li>
               <li>
                 <NavLink
-                to="/addItems"
-                  className={({isActive}) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-600" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  to="/addItems"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${
+                      isActive ? "text-orange-600" : "text-gray-700"
+                    } border-b border-gray-100 hover-bg-gray-50 lg:hover-bg-transparent lg-border-0 hover-text-orange-700 lg-p-0`
                   }
                 >
                   Sell Items
@@ -60,6 +80,7 @@ export default function Header() {
           </div>
         </div>
       </nav>
+      {showLogin && <Login onClose={closeLoginDialog} />}
     </header>
   );
 }
