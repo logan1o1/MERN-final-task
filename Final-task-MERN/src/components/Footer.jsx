f8109a5219ce3a93d75e3c630af671bb11b1edc4
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-11-07 21:32:13 +0530

    Update Footer.jsx

--- Final-task-MERN/src/components/Footer.jsx
+++ Final-task-MERN/src/components/Footer.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer className="bg-white border-y">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <Link to="/" className="flex items-center">
+              <img
+                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                className="mr-3 h-16"
+                alt="Logo"
+              />
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Resources
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <Link to="/" className="hover:underline">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/todayItems" className="hover:underline">
+                   Today's Items
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Follow us
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <a
+                    href="https://github.com/logan1o1/MERN-final-task"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Legal
+              </h2>
+              <ul className="text-gray-500 font-medium">
+                <li className="mb-4">
+                  <Link to="#" className="hover:underline">
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 text-center">
+            © 2023
+            . All Rights Reserved.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
 }
