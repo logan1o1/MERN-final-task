ddcde774b8b08b3b0f8bc71f376c24407f0f1b7f
Akshat <akshat9950@gmail.com>
2023-11-10 12:59:26 +0530

    Updated BG

--- Final-task-MERN/src/components/Home.jsx
+++ Final-task-MERN/src/components/Home.jsx
@@ -15,13 +15,14 @@ const Home = () => {
   }, []);
 
   const text =
-    "Welcome to [Auction Website Name], your one-stop shop for amazing deals and exciting auctions! Discover a wide variety of items up for bid, from electronics and collectibles to cars and antiques. With our user-friendly platform, you can easily browse, bid, and win the items you've always wanted. So start bidding today and see what treasures await you!";
+  "AuctionOFF.com, your one-stop shop for amazing deals and exciting auctions! Discover a wide variety of items up for bid, from electronics and collectibles to cars and antiques. With our user-friendly platform, you can easily browse, bid, and win the items you've always wanted. So start bidding today and see what treasures await you!";
+  
 
   const words = text.split(" ");
 
   return (
-    <BackgroundImage imageUrl="/Images/Bg.jpg">
-      <div className={`text-white absolute top-0 left-0 p-8 transition-opacity duration-1000 ${fadeIn ? "opacity-100" : "opacity-0"}`}>
+    <BackgroundImage imageUrl="/Images/NewBG2.png">
+      <div className={`text-black absolute top-1 left-0 p-8 transition-opacity duration-1000 ${fadeIn ? "opacity-100" : "opacity-0"}`}>
         <h1 className="font-bold text-2xl">
           {words.map((word, index) => (
             <span key={index} className="word">
