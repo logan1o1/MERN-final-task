e6800a42f8f4014ee1195a4e6f7f504dfa6b26e2
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-11-21 15:53:50 +0530

    vercel

--- MERN-backend/db/config.js
+++ MERN-backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://saswat:fSsUGnbYVx5W5vqx@e-auction.tnnygj8.mongodb.net/auction");
+mongoose.connect("mongodb+srv://saswat:fSsUGnbYVx5W5vqx@e-auction.tnnygj8.mongodb.net/auction?retryWrites=true&w=majority");
